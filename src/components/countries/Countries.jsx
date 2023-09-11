564fd6c3526fd442a8ff45c96756456275a49fe4
Md.Samsul Arefin <samsularefin532@gmail.com>
2023-09-11 13:57:41 +0600

    countries visited added

--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -9,14 +9,32 @@ const Countries = () => {
             .then(data => setCountries(data))
 
     }, [])
+    const [visitedcountries, setvisited] = useState([])
+    console.log(visitedcountries, "out")
+    const Visited = (para) => {
+        console.log(para)
+        console.log(visitedcountries, 'inside')
+        const newcountries = [...visitedcountries, para.name.common]
+        setvisited(newcountries)
+        // console.log(newcountries.length, newcountries)
+    }
     return (
         <>
             <h1>Total countries : {countries.length}</h1>
+            <h4>Number of Countries Visited:{visitedcountries.length}</h4>
+            <div className="flag-container"></div>
+            <div>
+                {
+
+                    visitedcountries.map(i => <li>{i}</li>)
+                }
+            </div>
             <div className="countries">
 
                 {
-                    countries.map(country => <Country key={country.cca3} country={country}></Country >)
+                    countries.map(country => <Country key={country.cca3} country={country} Visited={Visited}></Country >)
                 }
+
             </div >
         </>
     );
--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,17 +1,23 @@
 import { useState } from 'react';
 import './country.css'
 
-const Country = ({ country }) => {
+const Country = ({ country, Visited }) => {
     // const [name] = country;
     const { name, flags, popolation, area } = country;
     const [visit, setVisit] = useState(false)
     const Toggle = () => {
         setVisit(!visit)
     }
+    const markVisited = () => {
+        Visited(country);
+
+    }
 
     return (
-        <div className='country'>
-            <h4>name:{name?.common}</h4>
+        <div className={`country ${visit ? 'visited' : 'non'}`}>
+            <h4 style={{
+                color: visit ? 'white' : "red"
+            }}>name:{name?.common}</h4>
             <img src={flags.png} alt="" />
             <h4>population:{popolation}</h4>
             <h4>area:{area}</h4>
@@ -19,6 +25,7 @@ const Country = ({ country }) => {
             {
                 visit ? 'i have visited' : 'i will visit'
             }
+            <button onClick={markVisited} >Mark as visited</button>
         </div>
     )
 }
